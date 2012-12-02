575c8f7a38dea7a1bb63e27c95606db6cab7cd97
Gabriel Cziprusz <gcziprusz@gmail.com>
2012-12-02 17:08:19 -0600

    'spellcheck+GoogleAnalytics'

--- about.php
+++ about.php
@@ -7,7 +7,7 @@
 				I graduated from <a href="https://www.radford.edu" target="_blank">Radford University</a> with a double major in 
 				Software Engineering and Database Administration. During my studies, I had the opportunity to contribute to a 
 				small web marketing company called <a href="http://www.exemplum.com" target="_blank">Exemplum</a> as a part time 	
-				web developer, gainining valuable web development experience.			
+				web developer, gaining valuable web development experience.			
 				I am currently employed by 
 				<a target="_blank" href="http://www.icfi.com">ICF International</a>, a government consulting company based out of Fairfax, VA,
 				and work remotely as a web developer in Nashville, TN.
@@ -18,7 +18,7 @@
 				My goal is to work for a dynamic company dedicated to innovation, personal and professional development, and fair
 				business that allows for future growth and leadership opportunities.
  					
-				I am curretly seeking a position on a technical leadership team that encourages integration of communication and 
+				I am currently seeking a position on a technical leadership team that encourages integration of communication and 
 					technical skills to help shrink the gap between business and technology professionals.
 					</p>
 					<br/>
